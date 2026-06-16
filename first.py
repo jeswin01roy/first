ff3871b3fa153b354cdde425b80ecf13e3c457f3
jeswin01roy <135967640+jeswin01roy@users.noreply.github.com>
2026-06-16 12:17:05 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,7 @@
+n=int(input("enter the number terms:"))
+sum=0
+for i in range(0,n):
+     w=i+1
+     k=int(input("enter the number " + str(w) + ": "))
+     sum=sum+k
+print("the sum of the given numbeers are: ",sum)
